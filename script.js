cfa4da662531cf45e96783cf48bd980755d96cd4
mariya <mariya.podosinova@infinityworks.com>
2021-05-26 15:16:32 +0100

    add board and clicks funcrionality

--- script.js
+++ script.js
@@ -1,7 +1,89 @@
 const player1_btn = document.querySelector(".player1-btn");
 const player2_btn = document.querySelector(".player2-btn");
 
+let board = [
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+];
+let first = true;
+let gameOn = true;
+let winner = null;
+
+function takeTurn(row, column) {
+  console.log("takeTurn was called with row: " + row + ", column:" + column);
+
+  if (!gameOn) return null;
+  if (board[row][column] !== null) return board;
+
+  if (first) board[row][column] = "nought";
+  else board[row][column] = "cross";
+
+  first = !first;
+  return board;
+}
 function handleClick() {
   player1_btn.classList.toggle("active-btn");
   player2_btn.classList.toggle("active-btn");
+}
+
+// Clear down the elements drawn on the board.
+function clearBoard() {
+  for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
+      document.getElementById(
+        `row-${rowIndex}-column-${columnIndex}`
+      ).innerHTML = "";
+    }
+  }
+}
+
+// Populate the grid with images based on the board state.
+function drawBoard(board) {
+  clearBoard();
+  for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
+      if (!board[rowIndex][columnIndex]) {
+        continue;
+      }
+      document.getElementById(
+        `row-${rowIndex}-column-${columnIndex}`
+      ).innerText =
+        board[rowIndex][columnIndex] === "nought" ? "red" : "yellow";
+    }
+  }
+}
+// function isValidRowOrColumn(array) {
+//   return Array.isArray(array) && array.length === 6;
+// }
+
+// function isValidColumn(columnArray) {
+//   return isValidRowOrColumn(columnArray) && columnArray.every(function (item) { return ["nought", "cross", null].includes(item); });
+// }
+
+// A grid position was clicked call the game's turn function, redraw and then check for a winner.
+function positionClick(rowIndex, columnIndex, event) {
+  takeTurn(rowIndex, columnIndex);
+  const board = getBoard();
+
+  drawBoard(board);
+}
+function getBoard() {
+  return board;
+}
+
+// Bind the click events for the grid.
+for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+  for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
+    const gridPosition = document.getElementById(
+      `row-${rowIndex}-column-${columnIndex}`
+    );
+    gridPosition.addEventListener(
+      "click",
+      positionClick.bind(null, rowIndex, columnIndex)
+    );
+  }
 }
