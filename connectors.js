cab2eb8d1db5e1c672e245c055177f404df59af7
mariya <mariya.podosinova@infinityworks.com>
2021-06-04 16:13:41 +0100

    board is fine but not upload the screen

---
+++ connectors.js
@@ -0,0 +1,90 @@
+// A grid position was clicked call the game's turn function, redraw and then check for a winner.
+function positionClick(rowIndex, columnIndex, event) {
+  console.log(
+    "positionClick was called  with row: " +
+      rowIndex +
+      ", column:" +
+      columnIndex
+  );
+  takeTurn(dropToBottom(rowIndex, columnIndex), columnIndex);
+  const board = getBoard();
+
+  drawBoard(board);
+
+  const winner = checkWinner();
+  if (winner) {
+    if (
+      typeof winner !== "string" ||
+      !["red", "yellow", "nobody"].includes(winner)
+    ) {
+      throw (
+        "Expecting 'checkWinner' to return null or one of the strings 'noughts', 'crosses' or 'nobody'. Actually received: " +
+        winner
+      );
+    }
+    const winnerName = document.getElementById("winner-name");
+    winnerName.innerText = winner;
+    const winnerDisplay = document.getElementById("winner-display");
+    winnerDisplay.style.display = "block";
+  }
+}
+
+// Bind the click events for the grid.
+for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+  for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
+    const gridPosition = document.getElementById(
+      `row-${rowIndex}-column-${columnIndex}`
+    );
+
+    gridPosition.addEventListener(
+      "click",
+      positionClick.bind(null, rowIndex, columnIndex)
+    );
+  }
+}
+// Clear down the elements drawn on the board.
+function clearBoard() {
+  console.log("clearBoard was called ");
+  for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
+      document.getElementById(
+        `row-${rowIndex}-column-${columnIndex}`
+      ).innerHTML = "";
+    }
+  }
+}
+// Populate the grid with images based on the board state.
+function drawBoard(board) {
+  clearBoard();
+  console.log("drawBoard was called ", board);
+  for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
+      if (!board[rowIndex][columnIndex]) {
+        continue;
+      }
+
+      let currentClass =
+        board[rowIndex][columnIndex] === "red" ? "red" : "yellow";
+      document
+        .getElementById(
+          `row-${dropToBottom(rowIndex, columnIndex)}-column-${columnIndex}`
+        )
+        .classList.add(currentClass);
+    }
+  }
+}
+
+if (typeof exports === "object") {
+  console.log("Running in Node");
+  // Node. Does not work with strict CommonJS, but only CommonJS-like
+  // environments that support module.exports, like Node.
+  // eslint-disable-next-line no-undef
+  module.exports = {
+    positionClick,
+    drawBoard,
+    clearBoard,
+    resetClick,
+  };
+} else {
+  console.log("Running in Browser");
+}
